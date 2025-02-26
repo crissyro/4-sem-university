6b080b30fbb12123f9fb411ac4cf9ca6774ea248
Moroz Roman <morozdef24@gmail.com>
2025-02-26 21:21:50 +0300

    5 lab start

---
+++ oop/5_lab/credit_system.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+
+class Date {
+private:
+    u_int32_t day, month, year;
+
+public:
+    Date(u_int32_t d, u_int32_t m, u_int32_t y) : day(d), month(m), year(y) {}
+
+    inline u_int32_t getDay() const { return day; }
+    inline u_int32_t getMonth() const { return month; }
+    inline u_int32_t getYear() const { return year; }
+
+    bool operator==(const Date& other) const {
+        return day == other.day && month == other.month && year == other.year;
+    }
+
+};
+
+
+int main () {
+
+    return 0;
+}

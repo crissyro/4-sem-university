759892b8ac2972ae1aad007a752c8c3244f5871d
Moroz Roman <morozdef24@gmail.com>
2025-02-26 22:11:44 +0300

    5 lab abstract class finish

--- oop/5_lab/credit_system.cpp
+++ oop/5_lab/credit_system.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <iostream>
 #include <vector>
-#include <algorithm>
 #include <string>
 #include <iomanip>
+
 
 class Date {
 private:
@@ -15,8 +15,18 @@ public:
     inline u_int32_t getMonth() const { return month; }
     inline u_int32_t getYear() const { return year; }
 
-    bool operator==(const Date& other) const {
+    inline bool operator==(const Date& other) const {
         return day == other.day && month == other.month && year == other.year;
+    }
+
+    inline std::string toString() const {
+        std::ostringstream oss;
+        oss << std::setfill('0') 
+            << std::setw(2) << day << "."
+            << std::setw(2) << month << "."
+            << year;
+
+        return oss.str();
     }
 };
 
@@ -24,12 +34,103 @@ class Client {
 public: 
     virtual ~Client() = default;
     virtual void printInfo() const = 0;
-    virtual bool isCriteria() const = 0;
+    virtual bool isCriteria(const Date& targetDate) const = 0;
 };
 
+class Depositor: public Client {
+private:
+    std::string surname;
+    Date depositDate;
+    u_int64_t depositAmount;
+    _Float64 interestRate;
 
+public:
+    Depositor(const std::string& n, const Date& d, u_int64_t a, _Float64 r) : 
+            surname(n), depositDate(d), depositAmount(a), interestRate(r) {}
 
-int main () {
+    void printInfo() const override {
+        std::cout << "Тип: Вкладчик\nФамилия: " << surname
+        << "\nДата открытия: " << std::setfill('0') 
+        << std::setw(2) << depositDate.getDay() << "."
+        << std::setw(2) << depositDate.getMonth() << "."
+        << depositDate.getYear() << "\nСумма: " << depositAmount
+        << "\nПроцентная ставка: " << interestRate << "%\n\n";
+    }
+
+    inline bool isCriteria(const Date& targetDate) const override { return depositDate == targetDate; }
+};
+
+class Creditor : public Client {
+private:
+    std::string surname;
+    Date creditDate;
+    u_int64_t amount;
+    _Float64 interestRate;
+    _Float64 remainingDebt;
+
+public:
+    Creditor(const std::string& s, const Date& d, u_int64_t a, _Float64 ir, _Float64 rd) : 
+        surname(s), creditDate(d), amount(a), interestRate(ir), remainingDebt(rd) {}
+
+    void printInfo() const override {
+        std::cout << "Тип: Кредитор\nФамилия: " << surname
+            << "\nДата выдачи: " << std::setfill('0')
+            << std::setw(2) << creditDate.getDay() << "."
+            << std::setw(2) << creditDate.getMonth() << "."
+            << creditDate.getYear() << "\nСумма: " << amount
+            << "\nПроцентная ставка: " << interestRate << "%"
+            << "\nОстаток долга: " << remainingDebt << "\n\n";
+    }
+
+    inline bool isCriteria(const Date& targetDate) const override { return creditDate == targetDate; }
+};
+
+class Organization : public Client {
+    std::string name;
+    Date accountDate;
+    std::string accountNumber;
+    _Float64 balance;
+
+public:
+    Organization(std::string n, Date d, std::string an, double b)
+        : name(n), accountDate(d), accountNumber(an), balance(b) {}
+
+    void printInfo() const override {
+        std::cout << "Тип: Организация\nНазвание: " << name
+             << "\nДата открытия: " << std::setfill('0')
+             << std::setw(2) << accountDate.getDay() << "."
+             << std::setw(2) << accountDate.getMonth() << "."
+             << accountDate.getYear() << "\nНомер счёта: " << accountNumber
+             << "\nБаланс: " << balance << "\n\n";
+    }
+
+    inline bool isCriteria(const Date& targetDate) const override { return accountDate == targetDate; }
+};
+
+int main() {
+    std::vector<Client*> clients = {
+        new Depositor("Иваныч", {15, 5, 2025}, 510, 5.5),
+        new Creditor("Петрова", {15, 10, 2023}, 4000000, 7.0, 50000),
+        new Depositor("Сидорова", {10, 8, 2024}, 3500000, 6.2),
+        new Creditor("Левина", {10, 7, 2022}, 3000000, 6.8, 100000),
+        new Creditor("Мороз", {20, 6, 2021}, 1000000, 7.2, 250000),
+        new Organization("ООО \'БНАЛ\'", {10, 1, 2022}, "40702810500000000001", 2500000),
+        new Depositor("Мавроди", {15, 5, 2020}, 750000, 6.0)
+    };
+
+    std::cout << "Все клиенты:\n";
+
+    for (const auto& client : clients) { client->printInfo(); }
+
+    Date searchDate{15, 5, 2020};
+    std::cout << "\nКлиенты с датой сотрудничества 15.05.2020:\n";
+    for (const auto& client : clients) {
+        if (client->isCriteria(searchDate)) {
+            client->printInfo();
+        }
+    }
+
+    for (auto& client : clients) { delete client; }
 
     return 0;
 }

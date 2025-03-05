42b5ea6c75cd06a2f6de74dd2157f67db6ef9554
Moroz Roman <morozdef24@gmail.com>
2025-03-05 19:08:28 +0300

    lab 6 oop

---
+++ oop/6_lab/threads.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+
+class Progress {
+private:
+    u_int32_t progress;
+    std::mutex mtx;
+    
+public:
+
+
+};
+
+int main() {
+
+    return 0;
+}

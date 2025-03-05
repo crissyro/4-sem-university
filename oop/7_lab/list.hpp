a80fc39d6430dd93c446d43ded3ebdf74c267fa3
Moroz Roman <morozdef24@gmail.com>
2025-03-05 20:00:24 +0300

    start my list

---
+++ oop/7_lab/list.hpp
@@ -0,0 +1,17 @@
+#ifndef SRC_LIST__HPP
+#define SRC_LIST__HPP
+
+#include <algorithm>
+#include <initializer_list>
+#include <memory>
+#include <stdexcept>
+
+
+namespace mylist {
+
+
+
+}
+
+
+#endif // SRC_LIST__HPP

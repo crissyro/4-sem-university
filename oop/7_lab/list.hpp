0d1f44fa08c7a69181f63817de50c1c13e4a604c
Moroz Roman <morozdef24@gmail.com>
2025-03-05 20:07:06 +0300

    list 1

--- oop/7_lab/list.hpp
+++ oop/7_lab/list.hpp
@@ -9,7 +9,45 @@
 
 namespace mylist {
 
+template<typename T, typename Alloc = std::allocator<T>>
+class List {
+private:    
+    using alloc_traits = std::allocator_traits<Alloc>;
+    using value_type = T;
+    using size_type = std::size_t
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = typename alloc_traits::pointer;
+    using const_pointer = typename alloc_traits::const_pointer;
+    using iterator = *T;
+    using const_iterator = const *T;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+    struct Node {
+        value_type value;
+        Node* prev;
+        Node* next;
+        Node() : prev(nullptr), next(nullptr) {}
+        Node(value_type&& val, Node* p = nullptr, Node* n = nullptr) : value(std::move(val)), prev(p), next(n) {}
+        Node(const value_type& val, Node* p = nullptr, Node* n = nullptr) : value(val), prev(p), next(n) {}
+        ~Node() {
+            if (prev) prev->next = next;
+            if (next) next->prev = prev;
+            alloc_traits::destroy(allocator, &value);
+            alloc_traits::deallocate(allocator, this, 1);
+        }
+    }
+
+    Alloc allocator;
+    Node* head;
+    Node* tail;
+
+public:
+
+
+};
 
 }
 

a76ed43634c767bcffbabc583e64769e8e224ade
Moroz Roman <morozdef24@gmail.com>
2025-03-19 13:32:40 +0300

    btree 3

--- oop/8_lab/b_tree.cpp
+++ oop/8_lab/b_tree.cpp
@@ -13,3 +13,34 @@ BTree<T, Alloc>::~BTree() {
         alloc.deallocate(root, 1);
     }
 }
+
+template <typename T, typename Alloc>
+void BTree<T, Alloc>::deleteNode(Node* node) {
+    if(!node->is_leaf) {
+        for(auto child : node->children) {
+            deleteNode(child);
+            alloc.destroy(child);
+            alloc.deallocate(child, 1);
+        }
+    }
+}
+
+template <typename T, typename Alloc>
+void BTree<T, Alloc>::insert(const T& key) {
+    if(!root) {
+        root = alloc.allocate(1);
+        alloc.construct(root, true, alloc);
+        root->keys.push_back(key);
+        return;
+    }
+
+    if(root->keys.size() == 2*t-1) {
+        Node* new_root = alloc.allocate(1);
+        alloc.construct(new_root, false, alloc);
+        new_root->children.push_back(root);
+        splitChild(new_root, 0);
+        root = new_root;
+    }
+    
+    insertNonFull(root, key);
+}

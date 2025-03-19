72e1225a56aab360ee5dfa44458f4f5872bd0924
Moroz Roman <morozdef24@gmail.com>
2025-03-19 13:30:25 +0300

    btree

--- oop/8_lab/btree.hpp
+++ oop/8_lab/btree.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
-#include <vector>
 #include <memory>
+#include <vector>
+#include <iostream>
 #include <algorithm>
+#include <stdexcept>
 
+
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stdexcept>
 
 template <typename T, typename Alloc = std::allocator<T>>
 class BTree {
@@ -17,17 +25,25 @@ private:
 
     void splitChild(Node* parent, int index);
     void insertNonFull(Node* node, const T& key);
-    void balance(Node* node);
     void deleteNode(Node* node);
+    void balance(Node* node);
+    void borrowFromPrev(Node* node, int index);
+    void borrowFromNext(Node* node, int index);
+    void merge(Node* node, int index);
+    T getPredecessor(Node* node, int index);
+    T getSuccessor(Node* node, int index);
+    void fill(Node* node, int index);
+    void removeFromLeaf(Node* node, int index);
+    void removeFromNonLeaf(Node* node, int index);
+    Node* search(Node* node, const T& key) const;
 
 public:
     explicit BTree(int order = 4, const Alloc& alloc = Alloc());
-    
     ~BTree();
     
     void insert(const T& key);
     void erase(const T& key);
-    bool search(const T& key) const;
+    bool contains(const T& key) const;
     void print() const;
 };
 
@@ -35,7 +51,7 @@ template <typename T, typename Alloc>
 struct BTree<T, Alloc>::Node {
     bool is_leaf;
     std::vector<T, Alloc> keys;
-    std::vector<Node*> children;
+    std::vector<Node*, typename Alloc::template rebind<Node*>::other> children;
 
     Node(bool leaf, const Alloc& alloc) 
         : is_leaf(leaf), keys(alloc), children(alloc) {}

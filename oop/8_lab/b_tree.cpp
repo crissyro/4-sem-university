a9a63125b0e87bb2f4e97869d0e16707d12cd668
Moroz Roman <morozdef24@gmail.com>
2025-03-19 13:31:17 +0300

    btree 2

--- oop/8_lab/b_tree.cpp
+++ oop/8_lab/b_tree.cpp
@@ -4,3 +4,12 @@
 template <typename T, typename Alloc>
 BTree<T, Alloc>::BTree(int order, const Alloc& alloc) 
     : t(order/2), alloc(alloc), root(nullptr) {}
+
+template <typename T, typename Alloc>
+BTree<T, Alloc>::~BTree() {
+    if(root) {
+        deleteNode(root);
+        alloc.destroy(root);
+        alloc.deallocate(root, 1);
+    }
+}

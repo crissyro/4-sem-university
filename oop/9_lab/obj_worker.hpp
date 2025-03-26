382afa2f13b6471d55d830d31f48946276bcf5b9
Moroz Roman <morozdef24@gmail.com>
2025-03-26 17:23:09 +0300

    9 lab triangle

--- oop/9_lab/obj_worker.hpp
+++ oop/9_lab/obj_worker.hpp
@@ -44,8 +44,21 @@ private:
     u_int32_t v3;
     u_int32_t color;
 
-public:
+public: 
+    Triangle(u_int32_t v1 = 0, u_int32_t v2 = 0, u_int16_t v3 = 0) :
+        v1(v1), v2(v2), v3(v3), color(0) {}
 
+    bool operator==(const Triangle& other) const;
+    bool operator<(const Triangle& other) const;
 
+    inline u_int32_t getV1() const { return v1; }
+    inline u_int32_t getV2() const { return v2; }
+    inline u_int32_t getV3() const { return v3; }
+    inline u_int32_t getColor() const { return color; }
 
+    inline void setV1(u_int32_t v1) { this->v1 = v1; }
+    inline void setV2(u_int32_t v2) { this->v2 = v2; }
+    inline void setV3(u_int32_t v3) { this->v3 = v3; }
+    inline void setColor(u_int32_t color) { this->color = color; }
 };
+

bd7910a3f5bfb0191ffdb3bea89ad658bae33f79
Moroz Roman <morozdef24@gmail.com>
2025-03-26 17:40:37 +0300

    9 lab OBJModel

--- oop/9_lab/obj_worker.hpp
+++ oop/9_lab/obj_worker.hpp
@@ -20,6 +20,8 @@ public:
     Vertex(_Float32 x = 0, _Float32 y = 0, _Float32 z = 0) :
         x(x), y(y), z(z) {}
 
+    ~Vertex() = default;
+
     bool operator==(const Vertex& other) const;
 
     bool operator<(const Vertex& other) const;
@@ -48,6 +50,8 @@ public:
     Triangle(u_int32_t v1 = 0, u_int32_t v2 = 0, u_int16_t v3 = 0) :
         v1(v1), v2(v2), v3(v3), color(0) {}
 
+    ~Triangle() = default;
+
     bool operator==(const Triangle& other) const;
     bool operator<(const Triangle& other) const;
 
@@ -70,7 +74,16 @@ private:
     size_t findOrAddVertex(const Vertex& v);
 
 public:
-    
+    OBJModel() = default;
+    ~OBJModel() = default;
+
+    bool loadFromFile(const std::string& filename);
+    void saveToFile(const std::string& filename) const;
+    inline void setColor(const u_int32_t color);
+    void merge(OBJModel& other);
+    void sortVertices();
+    void sortTriangles();
+    void print() const;
 
 
 };

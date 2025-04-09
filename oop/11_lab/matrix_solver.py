40c449318d9496fbe630d26702eda71ce5ec5c01
Moroz Roman <morozdef24@gmail.com>
2025-04-09 11:29:10 +0300

    11 lab start

---
+++ oop/11_lab/matrix_solver.py
@@ -0,0 +1,9 @@
+from collections import defaultdict
+
+class Matrix:
+    def __init__(self, rows: int) -> None:
+        self.__rows = rows
+        
+    def find_sum_groups():
+        
+        
